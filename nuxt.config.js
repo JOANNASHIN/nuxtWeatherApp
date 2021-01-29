17627eec5b7889324019d3a96747214cfe82a02f
jieunshin <joannashin3@forbiz.co.kr>
2021-01-29 18:30:37 +0900

    icon변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,11 +44,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-      "~/plugins/main.js"
+      "~/plugins/global.js"
     // { src: '~/plugins/myPlugin.js'},
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  ],  
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
